95f619966b312d030d1aaad739d3fa96aebdb48c
jeffykle <jeffy.kle@gmail.com>
2021-02-09 09:17:56 -0600

    first commit

---
+++ app/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def default():
+    return render_template('index.html')
+
+
+@app.errorhandler(404)
+def not_found(err):
+    return render_template("404.html", err=err)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
